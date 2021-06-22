271fdf3109762e43d267c8107a036c77b7d47eba
SimpleExpenseManager <andrewson.18@cse.mrt.ac.lk>
2021-06-23 04:12:13 +0530

    adding env

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My key is {process.env.MY_KEY}
         </a>
       </header>
     </div>
